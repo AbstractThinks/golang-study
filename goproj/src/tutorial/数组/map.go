df7e2eaf4a71ca0a641ed2554c4551914a28d9ef
Jesse Zhuang <408455428@qq.com>
2017-01-05 09:29:11 +0800

    20170105

---
+++ goproj/src/tutorial/数组/map.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+        Lat, Long float64
+}
+
+var m map[string]Vertex
+
+func main() {
+        // map 在使用之前必须用 make 来创建；值为 nil 的 map 是空的，并且不能对其赋值。
+        m = make(map[string]Vertex)
+        m["Bell Labs"] = Vertex{40.68433, -74.399967}
+        fmt.Println(m["Bell Labs"])
+}
