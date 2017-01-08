8a0fb2caa79cd5f2e1987bdcfcd159e9bda0ae42
JesseZhuang <408455428@qq.com>
2017-01-08 21:31:57 +0800

    20170108

--- goproj/src/web/web服务/websocket/paserIP_demo.go
+++ goproj/src/web/web服务/websocket/paserIP_demo.go
@@ -8,14 +8,15 @@ import (
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		// fmt.Fprintln(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		os.Exit(1)
 	}
 	name := os.Args[1]
 	addr := net.ParseIP(name)
 	if addr == nil {
 		fmt.Println("Invalid address")
 	} else {
-		fmt.Println("The address is ", addr.String())
+		fmt.Println("the address is ", addr.String())
 	}
 	os.Exit(0)
 }
